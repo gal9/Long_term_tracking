ef7698066efc598930557e770cec0baa7aab281b
Gal petkovšek <gp1914@student.uni-lj.si>
2023-05-02 22:04:39 +0200

    folder name

--- eval_samples.py
+++ eval_samples.py
@@ -3,7 +3,7 @@ from eval_tracker import evaluate_tracker
 samples = [1, 3, 5, 10, 20, 50]
 
 for sample_n in samples:
-    res_name = "samples_" + str(sample_n)
-    print(sample_n, flush=True)
+    res_name = "./results/samples_" + str(sample_n)
+    print(res_name, flush=True)
 
     evaluate_tracker("./data", "models/siamfc_net.pth", res_name, visualize=False, search_num=sample_n)
--- eval_thresh.py
+++ eval_thresh.py
@@ -3,7 +3,7 @@ from eval_tracker import evaluate_tracker
 threshold_factors = [0.4, 0.5, 0.55, 0.6, 0.65, 0.7]
 
 for threshold_factor in threshold_factors:
-    res_name = "threshold_0" + str(threshold_factor)[2:]
+    res_name = "./results/threshold_0" + str(threshold_factor)[2:]
     print(threshold_factor, flush=True)
 
     evaluate_tracker("./data", "models/siamfc_net.pth", res_name, visualize=False, threshold_factor=threshold_factor)
